5e76966e589936564277a7eac2e9e0f4441f8d44
drexleradam <drexler1333@gmail.com>
2021-10-25 23:44:17 +0200

    Initial commit

---
+++ src/main/java/hu/alkund/cheeseback/CheeseBackApplication.java
@@ -0,0 +1,13 @@
+package hu.alkund.cheeseback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheeseBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheeseBackApplication.class, args);
+    }
+
+}
